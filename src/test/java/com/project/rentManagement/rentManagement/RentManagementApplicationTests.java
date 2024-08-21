f70637da21ec65868226e52f3bc4130c516eba42
Shubham <shubhambarve998@gmail.com>
2024-08-21 22:41:19 +0530

    Initial

---
+++ src/test/java/com/project/rentManagement/rentManagement/RentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.rentManagement.rentManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
